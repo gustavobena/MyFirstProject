a0a737eadb1fb0778e00a0e8eb4752075040ba58
gustavobena <54378794+gustavobena@users.noreply.github.com>
2019-08-28 13:50:46 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+	Your name
+	C++ Semester Year
+	Due: Due Date
+	Lab number and name
+	Lab description
+
+	Above is the format you should follow for all files you turn in. This area has
+	been setup as a multi-line comment because it is surrounded by a forward-slash-star
+	on the front and a star-forward-slash on the end. Comments are ignored by the
+	complier and used to help you and others to read and understand your code. I will
+	be adding comments to help you understand what the code contained in this file does.
+
+*/
+
+//	This is a comment. Single line comments begin with a double forward slash (//).
+
+/*	We are using a preprocessor directive here to include the iostream library. This is
+	the input/output stream library. There exist many premade/precomplied libraries that
+	can be helpful. We will extensively be using the collection of libraries known as
+	the standard library which includes iostream.
+*/
+#include <iostream>		
+
+int main()
+{
+
+	std::cout << "Hello World!\n";
+	std::cout << "My favorite color is green";
+
+
+	return 0;
+}
